114b35cbeb494347a47d83a59556b9a3d9f56a5f
MariAlipio <42498249+MariAlipio@users.noreply.github.com>
2018-09-30 10:08:28 -0300

    Add files via upload

---
+++ CONTAPOUPANCA/CONTAPOUPANCA/CONTAPOUPANCA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CONTAPOUPANCA
+{
+    class CONTAPOUPANCA
+    {
+        public static double taxadejurosanual { get; set; }
+        private double saldopoupanca;
+
+        public CONTAPOUPANCA() { }
+        public CONTAPOUPANCA(double SALDOpoupanca)
+        {
+            this.saldopoupanca = SALDOpoupanca;
+        }//FIM DOS METODOS CONSTRUTORES
+        public double Calcularjurosmensal()
+        {
+            //double juros = ((this.SALDOPoupanca * taxadejurosanual) / 12);
+            this.saldopoupanca += (this.saldopoupanca * taxadejurosanual) / 12;
+            return this.saldopoupanca;
+        }//FIM DOS METODOS ACESSORES
+
+    }
+}
---
+++ CONTAPOUPANCA/CONTAPOUPANCA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CONTAPOUPANCA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CONTAPOUPANCA.taxadejurosanual = 0.04;
+            CONTAPOUPANCA Poupador1 = new CONTAPOUPANCA(2000);
+            CONTAPOUPANCA Poupador2 = new CONTAPOUPANCA(3000);
+
+            Console.WriteLine("O saldo do poupador 1 é de:" + Poupador1.Calcularjurosmensal());
+            Console.WriteLine("O saldo do poupador 2 é de:" + Poupador2.Calcularjurosmensal());
+            
+            CONTAPOUPANCA.taxadejurosanual = 0.05;
+            Console.WriteLine("O saldo do poupador 1 é de:" + Poupador1.Calcularjurosmensal());
+            Console.WriteLine("O saldo do poupador 2 é de:" + Poupador2.Calcularjurosmensal());
+            Console.ReadKey();
+
+        }
+    }
+}
